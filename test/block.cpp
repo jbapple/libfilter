6622080f9da765d89f822985ac7f80c192348d49
Jim Apple <bloom@jbapple.com>
2020-05-25 08:58:09 -0700

    Add Simd-Scalar equivalence test
    The Simd and Scalar filter implementations should be identical to
    ensure that there are no semantic differences between filters that
    were run on different machines. SOmething similar to this actually
    came up in Apache Impala; see
    https://issues.apache.org/jira/browse/IMPALA-3817.
    
    This patch also adds comments to existing tests and also adds
    randomization to them.

--- test/block.cpp
+++ test/block.cpp
@@ -1,9 +1,12 @@
 #include "simd-block.hpp"
+using namespace filter;
+
+#include <vector>
+using namespace std;
 
 #include "gtest/gtest.h"
 
-using namespace filter;
-
+#include "util.hpp"
 
 template <typename F>
 class BlockTest : public ::testing::Test {};
@@ -28,22 +31,49 @@ using BlockTypes =
 
 TYPED_TEST_SUITE(BlockTest, BlockTypes);
 
+// Test that once something is inserted, it's always present
 TYPED_TEST(BlockTest, InsertPersists) {
   auto ndv = 8000;
   auto x = TypeParam::CreateWithBytes(ndv);
-  EXPECT_FALSE(x.FindHash(0));
+  vector<uint64_t> hashes(ndv);
+  Rand r;
   for (int i = 0; i < ndv; ++i) {
-    x.InsertHash(i);
+    hashes[i] = r();
+  }
+  for (int i = 0; i < ndv; ++i) {
+    x.InsertHash(hashes[i]);
     for (int j = 0; j <= i; ++j) {
-      EXPECT_TRUE(x.FindHash(j));
+      EXPECT_TRUE(x.FindHash(hashes[j]));
     }
   }
 }
 
+// Test that filters start with a 0.0 fpp.
 TYPED_TEST(BlockTest, StartEmpty) {
-  auto ndv = 8000;
+  auto ndv = 8000000;
   auto x = TypeParam::CreateWithBytes(ndv);
+  Rand r;
   for (int j = 0; j < ndv; ++j) {
-    EXPECT_FALSE(x.FindHash(j)) << j;
+    auto v = r();
+    EXPECT_FALSE(x.FindHash(v)) << v;
+  }
+}
+
+// Test that Scalar and Simd variants are identical
+TYPED_TEST(BlockTest, Buddy) {
+  if (TypeParam::is_simd) {
+    auto ndv = 800000;
+    auto x = TypeParam::CreateWithBytes(ndv);
+    auto y = TypeParam::Scalar::CreateWithBytes(ndv);
+    Rand r;
+    for (int i = 0; i < ndv; ++i) {
+      auto v = r();
+      x.InsertHash(v);
+      y.InsertHash(v);
+    }
+    for (int i = 0; i < ndv; ++i) {
+      auto v = r();
+      EXPECT_EQ(x.FindHash(v), y.FindHash(v));
+    }
   }
 }
