09a7771c705c3152c2f0a0b3def8cc83b95ed05a
Jim Apple <bloom@jbapple.com>
2020-09-10 21:32:33 -0700

    move towards using modules

--- java/LibraryTest.java
+++ java/LibraryTest.java
@@ -9,12 +9,14 @@ import static org.junit.Assert.*;
 public class LibraryTest {
   @Test
   public void testSomeLibraryMethod() {
-    libfilter.Library classUnderTest = new Library(123456);
+    //int x = 9;
+     libfilter.Library classUnderTest = new Library(123456);
     assertTrue(
         "someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     System.out.println("Before Add");
     classUnderTest.Add(867-5309);
     assertTrue("found 867-5309", classUnderTest.Find(867-5309));
+
     //assertFalse("did not found 867-5309", classUnderTest.Find(867-5309));
     //assertTrue("found 42", classUnderTest.Find(42));
   }
--- java/jni-bridge/Library.c
+++ java/jni-bridge/Library.c
@@ -8,7 +8,7 @@ JNIEXPORT
 void JNICALL Java_Library_doNothing(JNIEnv* a, jobject b) {}
 
 JNIEXPORT
-jboolean JNICALL Java_Library_Allocate(JNIEnv* a, jobject obj, jobject bb, jint bytes) {
+jboolean JNICALL Java_libfilter_Library_Allocate(JNIEnv* a, jobject obj, jobject bb, jint bytes) {
   // TODO: if the address is not aligned as we want, we probably need to memcpy
   // exit(1);
   // printf("uhoh\n");
@@ -16,20 +16,20 @@ jboolean JNICALL Java_Library_Allocate(JNIEnv* a, jobject obj, jobject bb, jint 
   return libfilter_block_init(bytes, me) >= 0;
 }
 
-JNIEXPORT jboolean JNICALL Java_Library_FindDetail(JNIEnv* a, jobject obj, jobject bb,
+JNIEXPORT jboolean JNICALL Java_libfilter_Library_FindDetail(JNIEnv* a, jobject obj, jobject bb,
                                                    jlong hashval) {
   // return false;
   const libfilter_block* me = (*a)->GetDirectBufferAddress(a, bb);
   return libfilter_block_find_hash(hashval, me);
 }
 
-JNIEXPORT void JNICALL Java_Library_AddDetail(JNIEnv* a, jobject obj, jobject bb,
+JNIEXPORT void JNICALL Java_libfilter_Library_AddDetail(JNIEnv* a, jobject obj, jobject bb,
                                               jlong hashval) {
   libfilter_block* me = (*a)->GetDirectBufferAddress(a, bb);
   libfilter_block_add_hash(hashval, me);
 }
 
-JNIEXPORT jboolean JNICALL Java_Library_Deallocate
+JNIEXPORT jboolean JNICALL Java_libfilter_Library_Deallocate
 (JNIEnv * env, jobject obj, jobject bb) {
   libfilter_block* me = (*env)->GetDirectBufferAddress(env, bb);
   return libfilter_block_destruct(me) >= 0;
