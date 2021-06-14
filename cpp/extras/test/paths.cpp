654adc2402f0094ab08c795a957c584482130c0f
Jim Apple <bloom@jbapple.com>
2021-06-14 13:56:40 -0700

    checkpoint

--- cpp/extras/test/paths.cpp
+++ cpp/extras/test/paths.cpp
@@ -14,44 +14,49 @@ using namespace std;
 #include "gtest/gtest.h"
 
 TEST(Main, FromTo) {
-  uint64_t entropy[4] = {1,0,1,0};
+  uint64_t entropy[4] = {1, 0, 1, 0};
   Feistel f(entropy);
   uint64_t xbase = 0x1234'5678'9abc'def0;
   auto low_level = 16;
   for (int i = 0; i < 64; ++i) {
-    for (int cursor = 0; cursor < 32; ++ cursor) {
-      auto x = xbase << i;
-      auto p = ToPath(x, f, cursor, low_level, false);
-      if (p.tail == 0) continue;
-      uint64_t y =
-          FromPathNoTail(p, f, low_level + (1 - p.long_fp), kHeadSize - (1 - p.long_fp));
-      int shift = 64 - (kLogLevels + low_level + kHeadSize);
-      EXPECT_EQ(x >> shift, y >> shift) << dec << i << endl
-                                        << shift << endl
-                                        << hex << (x >> shift) << endl
-                                        << (y >> shift);
+    for (int cursor = 0; cursor < 32; ++cursor) {
+      for (bool is_short : {true, false}) {
+        auto x = xbase << i;
+        auto p = ToPath(x, f, cursor, low_level, is_short);
+        if (p.tail == 0) continue;
+        uint64_t y = FromPathNoTail(p, f, low_level + (p.level < cursor),
+                                    kHeadSize - (1 - p.long_fp));
+        int shift = 64 - (kLogLevels + low_level + (p.level < cursor) + kHeadSize -
+                          (1 - p.long_fp));
+        EXPECT_EQ(x >> shift, y >> shift) << dec << i << endl
+                                          << shift << endl
+                                          << hex << (x >> shift) << endl
+                                          << (y >> shift);
+      }
     }
   }
 }
 
 TEST(Main, ToFromTo) {
-  uint64_t entropy[4] = {1,0,1,0};
+  uint64_t entropy[4] = {1, 0, 1, 0};
   Feistel f(entropy);
   uint64_t xbase = 0x1234'5678'9abc'def0;
   auto low_level = 16;
   for (int i = 0; i < 64; ++i) {
-    for (int cursor = 0; cursor < 32; ++ cursor) {
-    auto x = xbase << i;
-    auto p = ToPath(x, f, cursor, low_level, false);
-    if (p.tail == 0) continue;
-    uint64_t y =
-        FromPathNoTail(p, f, low_level + (1 - p.long_fp), kHeadSize - (1 - p.long_fp));
-    Path q = ToPath(y, f, cursor, low_level, false);
-    EXPECT_EQ(p.level, q.level);
-    EXPECT_EQ(p.bucket, q.bucket);
-    EXPECT_EQ(p.fingerprint, q.fingerprint);
-    EXPECT_EQ(p.long_fp, q.long_fp);
-    //EXPECT_EQ(0u, q.tail);
+    for (int cursor = 0; cursor < 32; ++cursor) {
+      for (bool is_short : {true, false}) {
+        auto x = xbase << i;
+        auto p = ToPath(x, f, cursor, low_level, is_short);
+        if (p.tail == 0) continue;
+        uint64_t y = FromPathNoTail(p, f, low_level + (p.level < cursor),
+                                    kHeadSize - (1 - p.long_fp));
+        Path q = ToPath(y, f, cursor, low_level, is_short);
+        EXPECT_EQ(p.level, q.level);
+        EXPECT_EQ(p.bucket, q.bucket);
+        EXPECT_EQ(p.fingerprint, q.fingerprint);
+        EXPECT_EQ(p.long_fp, q.long_fp);
+        // EXPECT_EQ(0u, q.tail);
+      }
     }
   }
 }
