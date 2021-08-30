10e83d0238df8b01d1e10b999cfb775ba4576e24
Jim Apple <bloom@jbapple.com>
2021-08-30 07:55:42 -0700

    add union to tcf

--- cpp/extras/include/util.hpp
+++ cpp/extras/include/util.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
 #include <cstdint>
+#include <iostream>
 #include <random>
 
+using namespace std;
 
 // Generator for unsigned 64-bit ints, initialized from system's randomness generator.
 class Rand {
@@ -38,8 +40,46 @@ public:
     yState = yState << 32;
     yState |= raw();
 
+    // 5
+    // xState = 0x65d669960f333899;
+    // yState = 0x74356b1ce0304637;
+
+    // 4
+    // xState = 0xd577603879438a27;
+    // yState = 0xe2679c8afa199898;
+
+    // 8
+    // xState = 0x46d18fbe1e4ebc67;
+    // yState = 0x941d540452ab38aa;
+
+    // 1 14
+    // 64 58
+    // xState = 0x25432790d7657885;
+    // yState = 0x6b4ce3189fd1dc1f;
+
+    // 62 28
+    // xState = 0x413db91a87e812c8;
+    // yState = 0x44b9fde1f7da9d96;
+
+    // ndv 14 12
+    // 300a7748208328a4
+    // 2da460f286315a88
+
+    // ndv 11 12
+    // d47e8e2365691c80
+    // e4861292dabc35a3
+
+    // ndv 10 10
+    // 9340b1b8521d6da7
+    // 2adb57414584700e
+
+    // ndv 9 9
+    // xState = 0x5389ca9d7e980a03;
+    // yState = 0x87d5ae2ea5ff2a26;
+
     initXState = xState;
     initYState = yState;
+    // cout << hex << xState << "\n" << yState << endl;
   }
 
   std::uint64_t operator()() { return romuDuoJr_random(); }
