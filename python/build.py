8d66e7c1738da7f553ff62e86acd4c90f438c981
Jim Apple <bloom@jbapple.com>
2022-04-17 10:03:27 -0700

    use installed library if possible

--- python/build.py
+++ python/build.py
@@ -8,7 +8,6 @@ ffibuilder.set_source("libfilter",
                       #include "filter/taffy-cuckoo.h"
                       """,
                       include_dirs=["../c/include"],
-                      library_dirs=["../c/lib"],
                       libraries=["filter"],
                       extra_compile_args=["-march=native"],
                       extra_link_args=["-Wl,-rpath,../c/lib"])
