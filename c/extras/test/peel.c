7e60b5fbf854e681acc4efaa56aeac3951dc7edf
Jim Apple <bloom@jbapple.com>
2022-06-21 18:01:52 -0700

    add tests; fix a couple of bugs

--- c/extras/test/peel.c
+++ c/extras/test/peel.c
@@ -6,58 +6,87 @@
 #include <string.h>
 #include <sys/random.h>
 
-// size_t libfilter_peel_test(size_t n, size_t m) {
-// retry:;
-//   uint64_t* hashes = malloc(n * sizeof(uint64_t));
-//   size_t to_fill = n * sizeof(uint64_t);
-//   while (to_fill > 0) {
-//     const size_t filled =
-//         getrandom(&((char*)hashes)[n * sizeof(uint64_t) - to_fill], to_fill, 0);
-//     to_fill = to_fill - filled;
-//     printf("filled %lu out of %lu\n", n * sizeof(uint64_t) - to_fill,
-//            n * sizeof(uint64_t));
-//     // printf("%s\n", strerror(errno));
-//   }
+size_t libfilter_round_trip_test(size_t n, size_t m) {
+retry:;
+  uint64_t* hashes = malloc(n * sizeof(uint64_t));
+  size_t to_fill = n * sizeof(uint64_t);
+  while (to_fill > 0) {
+    const size_t filled =
+        getrandom(&((char*)hashes)[n * sizeof(uint64_t) - to_fill], to_fill, 0);
+    to_fill = to_fill - filled;
+    printf("filled %lu out of %lu\n", n * sizeof(uint64_t) - to_fill,
+           n * sizeof(uint64_t));
+    // printf("%s\n", strerror(errno));
+  }
 
-//   libfilter_edge* edges = libfilter_init_edges(n, m, hashes);
-//   free(hashes);
-//   libfilter_peel_node* nodes = libfilter_init_peel_nodes(m);
-//   libfilter_populate_peel_nodes(n, edges, nodes);
-//   uint64_t* result = malloc(m * sizeof(uint64_t));
-//   uint8_t* xors = calloc(m, sizeof(uint8_t));
-//   size_t answer =  libfilter_peel(n, m, edges, nodes, result, xors);
-//   if (answer < m) {
-//     free(xors);
-//     free(result);
-//     free(nodes);
-//     free(edges);
-//     goto retry;
-//   }
-//   for (size_t i = 0; i < n; ++i) {
-//     printf("%ld\n", i);
-//     assert(libfilter_find_edge(&edges[i], xors));
-//   }
-//   free(xors);
-//   free(result);
-//   for (size_t i = 0; i < m; ++i) {
-//     free(nodes[i].edges_);
-//   }
-//   free(nodes);
-//   free(edges);
-//   return answer;
-// }
+  libfilter_edge* edges = libfilter_init_edges(n, m, hashes);
+  free(hashes);
+  libfilter_peel_node* nodes = libfilter_init_peel_nodes(m);
+  libfilter_populate_peel_nodes(n, edges, nodes);
+  libfilter_edge_peel* result = malloc(m * sizeof(libfilter_edge_peel));
+  uint8_t* xors = calloc(m, sizeof(uint8_t));
+  size_t answer = libfilter_peel(m, edges, nodes, result);
+  printf("peeled %ld\n", answer);
+  if (answer < m) {
+    free(xors);
+    free(result);
+    free(nodes);
+    free(edges);
+    goto retry;
+  }
+  libfilter_unpeel(m, edges, result, xors);
+  for (size_t i = 0; i < n; ++i) {
+    //printf("%ld\n", i);
+    assert(libfilter_find_edge(&edges[i], xors));
+  }
+  free(xors);
+  free(result);
+  free(nodes);
+  free(edges);
+  return answer;
+}
 
-void libfilter_unpeel_test_one(size_t edge_count, const libfilter_edge* edges,
-                               const libfilter_edge_peel* peel_order, uint8_t* xors) {
+void libfilter_unpeel_test_reverse(size_t edge_count, const libfilter_edge* edges,
+                                   const libfilter_edge_peel* peel_order, uint8_t* xors) {
   libfilter_unpeel(edge_count, edges, peel_order, xors);
   for (size_t i = 0; i < edge_count; ++i) {
-    uint8_t xor_remainder = 0;
-    for (int k = 0; k < LIBFILTER_EDGE_ARITY; ++k) {
-      //if (edges[edge_order[j]].vertex_[k] == edge_last_nodes[j]) continue;
-      xor_remainder ^= xors[edges[peel_order[i].edge_number_].vertex_[k]];
-    }
-    assert(edges[peel_order[i].edge_number_].fingerprint_ == xor_remainder);
+    assert(libfilter_find_edge(&edges[peel_order[i].edge_number_], xors));
   }
+}
+
+void libfilter_peel_once_test_0() {
+  libfilter_edge e0 = {.vertex_ = {0, 1, 2}, .fingerprint_ = 1};
+  libfilter_edge e1 = {.vertex_ = {1, 2, 3}, .fingerprint_ = 2};
+  libfilter_edge edges[] = {e0, e1};
+  libfilter_edge_peel peel_order[] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
+  libfilter_peel_node peel_nodes[] = {{1, 0}, {2, 1}, {2, 1}, {1, 1}};
+  assert(2 == libfilter_peel_once(0, edges, peel_nodes, peel_order));
+  assert(peel_order[0].edge_number_ == 1);
+  assert(peel_order[0].peeled_vertex_ == 1);
+  assert(peel_order[1].edge_number_ == 1);
+  assert(peel_order[1].peeled_vertex_ == 2);
+  assert(0 == libfilter_peel_once(1, edges, peel_nodes, &peel_order[2]));
+  assert(peel_nodes[0].count_ == 0);
+  assert(peel_nodes[1].count_ == 0);
+  assert(peel_nodes[2].count_ == 0);
+  assert(peel_nodes[3].count_ == 0);
+}
+
+void libfilter_peel_test_0() {
+  libfilter_edge e0 = {.vertex_ = {0, 1, 2}, .fingerprint_ = 1};
+  libfilter_edge e1 = {.vertex_ = {1, 2, 3}, .fingerprint_ = 2};
+  const libfilter_edge edges[] = {e0, e1};
+  libfilter_edge_peel peel_order[] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
+  libfilter_peel_node peel_nodes[] = {{1, 0}, {2, 1}, {2, 1}, {1, 1}};
+  assert(4 == libfilter_peel(4, edges, peel_nodes, peel_order));
+  assert(peel_order[0].edge_number_ == 0);
+  assert(peel_order[0].peeled_vertex_ == 0);
+  assert(peel_order[1].edge_number_ == 1);
+  assert(peel_order[1].peeled_vertex_ == 3);
+  assert(peel_order[2].edge_number_ == 1);
+  assert(peel_order[2].peeled_vertex_ == 1);
+  assert(peel_order[3].edge_number_ == 1);
+  assert(peel_order[3].peeled_vertex_ == 2);
 }
 
 void libfilter_unpeel_test_0() {
@@ -66,9 +95,9 @@ void libfilter_unpeel_test_0() {
   libfilter_edge edges[] = {e0, e1};
   libfilter_edge_peel peel_order[] = {{0, 0}, {1, 3}};
   uint8_t xors[] = {0, 0, 0, 0};
-  libfilter_unpeel_test_one(1, edges, peel_order, xors);
+  libfilter_unpeel_test_reverse(1, edges, peel_order, xors);
   xors[0] = xors[1] = xors[2] = xors[3] = 0;
-  libfilter_unpeel_test_one(2, edges, peel_order, xors);
+  libfilter_unpeel_test_reverse(2, edges, peel_order, xors);
 }
 
 void libfilter_unpeel_test_1() {
@@ -78,23 +107,119 @@ void libfilter_unpeel_test_1() {
   libfilter_edge edges[] = {e0, e1, e2};
   libfilter_edge_peel peel_order[] = {{0, 0}, {2, 4}, {1, 2}};
   uint8_t xors[] = {0, 0, 0, 0, 0};
-  libfilter_unpeel_test_one(1, edges, peel_order, xors);
+  libfilter_unpeel_test_reverse(1, edges, peel_order, xors);
   xors[0] = xors[1] = xors[2] = xors[3] = xors[4] = 0;
-  libfilter_unpeel_test_one(2, edges, peel_order, xors);
+  libfilter_unpeel_test_reverse(2, edges, peel_order, xors);
   xors[0] = xors[1] = xors[2] = xors[3] = xors[4] = 0;
-  libfilter_unpeel_test_one(3, edges, peel_order, xors);
+  libfilter_unpeel_test_reverse(3, edges, peel_order, xors);
 }
 
-void  libfilter_unpeel_test() {
-  libfilter_unpeel_test_0();
-  // libfilter_unpeel_test_1();
+void libfilter_peel_once_test_1() {
+  libfilter_edge e0 = {.vertex_ = {0, 1, 2}, .fingerprint_ = 1};
+  libfilter_edge e1 = {.vertex_ = {1, 2, 3}, .fingerprint_ = 2};
+  libfilter_edge e2 = {.vertex_ = {2, 3, 4}, .fingerprint_ = 2};
+  libfilter_edge edges[] = {e0, e1, e2};
+  libfilter_edge_peel peel_order[] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}};
+  libfilter_peel_node peel_nodes[] = {
+      {1, 0}, {2, 0 ^ 1}, {3, 0 ^ 1 ^ 2}, {2, 1 ^ 2}, {1, 2}};
+  assert(1 == libfilter_peel_once(0, edges, peel_nodes, peel_order));
+  assert(peel_order[0].edge_number_ == 1);
+  assert(peel_order[0].peeled_vertex_ == 1);
+  assert(2 == libfilter_peel_once(4, edges, peel_nodes, &peel_order[1]));
+  assert(peel_order[1].edge_number_ == 1);
+  assert(peel_order[1].peeled_vertex_ == 2);
+  assert(peel_order[2].edge_number_ == 1);
+  assert(peel_order[2].peeled_vertex_ == 3);
+  assert(0 == libfilter_peel_once(1, edges, peel_nodes, &peel_order[3]));
+
+  assert(peel_nodes[0].count_ == 0);
+  assert(peel_nodes[1].count_ == 0);
+  assert(peel_nodes[2].count_ == 0);
+  assert(peel_nodes[3].count_ == 0);
+  assert(peel_nodes[4].count_ == 0);
 }
+
+void libfilter_peel_test_1() {
+  libfilter_edge e0 = {.vertex_ = {0, 1, 2}, .fingerprint_ = 1};
+  libfilter_edge e1 = {.vertex_ = {1, 2, 3}, .fingerprint_ = 2};
+  libfilter_edge e2 = {.vertex_ = {2, 3, 4}, .fingerprint_ = 2};
+  const libfilter_edge edges[] = {e0, e1, e2};
+  libfilter_edge_peel peel_order[] = {{0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}};
+  libfilter_peel_node peel_nodes[] = {
+      {1, 0}, {2, 0 ^ 1}, {3, 0 ^ 1 ^ 2}, {2, 1 ^ 2}, {1, 2}};
+  assert(5 == libfilter_peel(5, edges, peel_nodes, peel_order));
+  assert(peel_order[0].edge_number_ == 0);
+  assert(peel_order[0].peeled_vertex_ == 0);
+  assert(peel_order[1].edge_number_ == 2);
+  assert(peel_order[1].peeled_vertex_ == 4);
+  assert(peel_order[2].edge_number_ == 1);
+  assert(peel_order[2].peeled_vertex_ == 1);
+  assert(peel_order[3].edge_number_ == 1);
+  assert(peel_order[3].peeled_vertex_ == 2);
+  assert(peel_order[4].edge_number_ == 1);
+  assert(peel_order[4].peeled_vertex_ == 3);
+}
+
+void libfilter_unpeel_test_2() {
+  libfilter_edge e0 = {.vertex_ = {0, 1, 2}, .fingerprint_ = 1};
+  libfilter_edge e1 = {.vertex_ = {1, 2, 3}, .fingerprint_ = 2};
+  libfilter_edge e2 = {.vertex_ = {2, 3, 4}, .fingerprint_ = 2};
+  libfilter_edge edges[] = {e0, e1, e2};
+  libfilter_edge_peel peel_order[] = {{0, 0}, {2, 4}, {1, 1}, {1, 2}, {1, 3}};
+  uint8_t xors[] = {0, 0, 0, 0, 0};
+  libfilter_unpeel_test_reverse(1, edges, peel_order, xors);
+  xors[0] = xors[1] = xors[2] = xors[3] = xors[4] = 0;
+  libfilter_unpeel_test_reverse(2, edges, peel_order, xors);
+  xors[0] = xors[1] = xors[2] = xors[3] = xors[4] = 0;
+  libfilter_unpeel_test_reverse(3, edges, peel_order, xors);
+  xors[0] = xors[1] = xors[2] = xors[3] = xors[4] = 0;
+  libfilter_unpeel_test_reverse(4, edges, peel_order, xors);
+  xors[0] = xors[1] = xors[2] = xors[3] = xors[4] = 0;
+  libfilter_unpeel_test_reverse(5, edges, peel_order, xors);
+}
+
+void libfilter_unpeel_test() {
+  libfilter_unpeel_test_0();
+  libfilter_unpeel_test_1();
+  libfilter_unpeel_test_2();
+}
+
+void libfilter_peel_once_test() {
+  libfilter_peel_once_test_0();
+  libfilter_peel_once_test_1();
+}
+
+void libfilter_peel_test() {
+  libfilter_peel_test_0();
+  libfilter_peel_test_1();
+}
+
+void libfilter_full_test_1() {
+  libfilter_edge e0 = {.vertex_ = {0, 1, 2}, .fingerprint_ = 1};
+  libfilter_edge e1 = {.vertex_ = {1, 2, 3}, .fingerprint_ = 2};
+  libfilter_edge e2 = {.vertex_ = {2, 3, 4}, .fingerprint_ = 2};
+  const libfilter_edge edges[] = {e0, e1, e2};
+  libfilter_edge_peel peel_order[5];
+  libfilter_peel_node* peel_nodes = libfilter_init_peel_nodes(5);
+  libfilter_populate_peel_nodes(3, edges, peel_nodes);
+  libfilter_peel(5, edges, peel_nodes, peel_order);
+  uint8_t xors[5] = {};
+  libfilter_unpeel_test_reverse(3, edges, peel_order, xors);
+}
+
+void libfilter_full_test() {
+  libfilter_full_test_1();
+}
+
 
 int main() {
-  // size_t n, m;
-  // scanf("%ld", &n);
-  // scanf("%ld", &m);
-
   libfilter_unpeel_test();
-  // printf("%ld\n", libfilter_peel_test(n, m));
+  libfilter_peel_once_test();
+  libfilter_peel_test();
+  libfilter_full_test();
+
+  size_t n, m;
+  scanf("%ld", &n);
+  scanf("%ld", &m);
+  printf("%ld\n", libfilter_round_trip_test(n, m));
 }
--- c/include/filter/peel.h
+++ c/include/filter/peel.h
@@ -12,13 +12,15 @@ typedef struct {
   uint8_t fingerprint_;
 } libfilter_edge;
 
-bool libfilter_in_edge(size_t v, int seen, const size_t vertex_[LIBFILTER_EDGE_ARITY]) {
+// return if v is among the first seen vertexes in vertex
+bool libfilter_in_edge(size_t v, int seen, const size_t vertex[LIBFILTER_EDGE_ARITY]) {
   for (int i = 0; i < seen; ++i) {
-    if (v == vertex_[i]) return true;
+    if (v == vertex[i]) return true;
   }
   return false;
 }
 
+// makes a hyperedge from hash where each vertex is less than m
 void libfilter_make_edge(uint64_t hash, size_t m, libfilter_edge* result) {
   for (int j = 0; j < LIBFILTER_EDGE_ARITY; ++j) {
     const unsigned __int128 tmp = (unsigned __int128)hash * (unsigned __int128)m;
@@ -28,12 +30,14 @@ void libfilter_make_edge(uint64_t hash, size_t m, libfilter_edge* result) {
       result->vertex_[j] += (result->vertex_[j] == m) ? (-m) : 0;
     }
     assert(result->vertex_[j] < m);
+    hash = hash * m;
+    //printf("%ld, ", result->vertex_[j]);
   }
   result->fingerprint_ = hash >> (64 - 8);
+  //printf("\n");
 }
 
-// Make random edges
-// TODO: what if two nodes coincide
+// Make n random edges from hashes with nodes less than m
 libfilter_edge* libfilter_init_edges(size_t n, size_t m, const uint64_t* hashes) {
   libfilter_edge* result = malloc(n * sizeof(libfilter_edge));
   if (NULL == result) return result;
@@ -41,6 +45,7 @@ libfilter_edge* libfilter_init_edges(size_t n, size_t m, const uint64_t* hashes)
   return result;
 }
 
+// return true if the fingerprint of edge matches the expected fingerprint in xors
 bool libfilter_find_edge(const libfilter_edge* edge, const uint8_t* xors) {
   uint8_t fingerprint = edge->fingerprint_;
   for (int i = 0; i < LIBFILTER_EDGE_ARITY; ++i) {
@@ -49,19 +54,22 @@ bool libfilter_find_edge(const libfilter_edge* edge, const uint8_t* xors) {
   return 0 == fingerprint;
 }
 
+// structure temporarily used for peeling a hypergraph
 typedef struct {
-  size_t count_;
-  uint64_t edges_;
+  size_t count_;   // number of hyperedges incident to this node
+  uint64_t edges_; // xor of the remaining hyperedges incident to this node
 } libfilter_peel_node;
 
+// Allocate and zero-initialize libfilter_peel_node array of size m
 libfilter_peel_node* libfilter_init_peel_nodes(size_t m) {
   libfilter_peel_node* result = calloc(m, sizeof(libfilter_peel_node));
   return result;
 }
 
-void libfilter_populate_peel_nodes(size_t n, const libfilter_edge* edges,
+// take n edges and initialize nodes
+void libfilter_populate_peel_nodes(size_t num_edges, const libfilter_edge* edges,
                                    libfilter_peel_node* nodes) {
-  for (size_t i = 0; i < n; ++i) {
+  for (size_t i = 0; i < num_edges; ++i) {
     for(int j = 0; j < LIBFILTER_EDGE_ARITY; ++j) {
       libfilter_peel_node * node = &nodes[edges[i].vertex_[j]];
       node->count_++;
@@ -70,19 +78,20 @@ void libfilter_populate_peel_nodes(size_t n, const libfilter_edge* edges,
   }
 }
 
+// strcture temporarily used for tracking the order of peeling
 typedef struct {
   size_t edge_number_;
-  size_t peeled_vertex_;
+  size_t peeled_vertex_; // the vertex this edge was peeled at
 } libfilter_edge_peel;
 
-// returns number of new peelable vertexes uncovered. Always less than
+// peel vetex_number, given edges and nodes.
+// returns number of new peelable vertexes uncovered. Always strictly less than
 // LIBFILTER_EDGE_ARITY. Adds new peelable nodes to to_peel.
-size_t libfilter_peel_once(size_t n, uint64_t vertex_number, const libfilter_edge* edges,
+size_t libfilter_peel_once(uint64_t vertex_number, const libfilter_edge* edges,
                            libfilter_peel_node* nodes, libfilter_edge_peel* to_peel) {
   size_t result = 0;
   assert(nodes[vertex_number].count_ == 1);
   const uint64_t edge_number = nodes[vertex_number].edges_;
-  assert(edge_number < n);
   for (int i = 0; i < LIBFILTER_EDGE_ARITY; ++i) {
     const size_t vertex = edges[edge_number].vertex_[i];
     nodes[vertex].edges_ ^= edge_number;
@@ -97,37 +106,44 @@ size_t libfilter_peel_once(size_t n, uint64_t vertex_number, const libfilter_edg
   return result;
 }
 
-// // returns number of nodes peeled
-// size_t libfilter_peel(size_t n, size_t m, const libfilter_edge* edges,
-//                       libfilter_peel_node* nodes, uint64_t* vertex_out,
-//                       uint64_t* edge_out) {
-//   uint64_t begin_stack = 0, end_stack = 0, to_see = 0;
-//   while (to_see < m) {
-//     if (nodes[to_see].count_ > 1) {
-//       ++to_see;
-//       continue;
-//     }
-//     vertex_out[end_stack++] = to_see++;
-//   }
-//   while (begin_stack < end_stack) {
-//     // invariant:
-//     assert(nodes[vertex_out[begin_stack]].count_ < 2);
-//     if (nodes[vertex_out[begin_stack]].count_ == 0) {
-//       ++begin_stack;
-//       continue;
-//     }
-//     *edge_out++ = nodes[vertex_out[begin_stack]].edges_[0];
-//     end_stack += libfilter_peel_once(n, vertex_out[begin_stack], edges, nodes,
-//                                      &vertex_out[end_stack]);
-//     ++begin_stack;
-//   }
-//   return begin_stack;
-// }
+// returns number of nodes peeled
+size_t libfilter_peel(size_t num_nodes, const libfilter_edge* edges,
+                      libfilter_peel_node* nodes, libfilter_edge_peel* to_peel) {
+  uint64_t begin_stack = 0, end_stack = 0, to_see = 0;
+  while (to_see < num_nodes) {
+    if (nodes[to_see].count_ > 1) {
+      ++to_see;
+      continue;
+    }
+    to_peel[end_stack].edge_number_ = nodes[to_see].edges_;
+    to_peel[end_stack].peeled_vertex_ = to_see;
+    ++end_stack;
+    ++to_see;
+  }
+  while (begin_stack < end_stack) {
+    // invariant:
+    assert(nodes[to_peel[begin_stack].peeled_vertex_].count_ < 2);
+    if (nodes[to_peel[begin_stack].peeled_vertex_].count_ == 0) {
+      ++begin_stack;
+      continue;
+    }
+    //*edge_out++ = nodes[to_peel[begin_stack]].edges_;
+    end_stack += libfilter_peel_once(to_peel[begin_stack].peeled_vertex_, edges, nodes,
+                                     &to_peel[end_stack]);
+    ++begin_stack;
+  }
+  if (begin_stack != num_nodes) {
+    for (size_t i = 0; i < num_nodes; ++i) {
+      assert(nodes[i].count_ != 1);
+    }
+  }
+  return begin_stack;
+}
 
-void libfilter_unpeel(size_t edge_count, const libfilter_edge* edges,
+void libfilter_unpeel(size_t node_count, const libfilter_edge* edges,
                       const libfilter_edge_peel* peel_order, uint8_t* xors) {
-  for (size_t i = 0; i < edge_count; ++i) {
-    size_t j = edge_count - 1 - i;
+  for (size_t i = 0; i < node_count; ++i) {
+    size_t j = node_count - 1 - i;
     uint8_t xor_remainder = edges[peel_order[j].edge_number_].fingerprint_;
     assert(0 == xors[peel_order[j].peeled_vertex_]);
     for (int k = 0; k < LIBFILTER_EDGE_ARITY; ++k) {
