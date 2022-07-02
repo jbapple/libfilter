c221f8b7d65ba5fa450aa592e98eaebd16c404ba
Jim Apple <bloom@jbapple.com>
2022-07-02 10:54:44 -0700

    fix clone in static python

--- python/static.py
+++ python/static.py
@@ -3,16 +3,15 @@ from libfilter import ffi, lib
 class Static:
   def __init__(self, hashes):
     self.b = lib.libfilter_static_construct(len(hashes), hashes)
-    self.b = ffi.gc(self.b, lib.libfilter_static_destroy, (int)(len(hashes) * 1.23))
+    self.b = ffi.gc(self.b, lib.libfilter_static_destroy, self.b.length_)
 
   def __contains__(self, hash):
     return lib.libfilter_static_lookup(self.b, hash)
 
   def clone(self):
-    result = lib.libfilter_static_clone(self.b, result)
-    result = ffi.gc(result, lib.libfilter_static_destruct,
-                    lib.libfilter_static_size_in_bytes(result))
-    answer = Static()
+    result = lib.libfilter_static_clone(self.b)
+    result = ffi.gc(result, lib.libfilter_static_destroy, result.length_)
+    answer = Static([])
     answer.b = result
     return answer
 
